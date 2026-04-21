73b9bd032fdfbf829b74adf48b30e01ccabef876
Mark Qvist <mark@unsigned.io>
2026-04-21 10:51:25 +0200

    Updated versions

--- setup.py
+++ setup.py
@@ -114,8 +114,8 @@ setuptools.setup(
         ]
     },
     install_requires=[
-        "rns>=1.1.6",
-        "lxmf>=0.9.4",
+        "rns>=1.1.7",
+        "lxmf>=0.9.5",
         "lxst>=0.4.6",
         "kivy>=2.3.0",
         "numpy>=2.0.0",
